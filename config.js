0c6af2a206eae56f0d72a5eea60e2d37eba73705
HRBsupport <wakeoneteam@gmail.com>
2025-02-21 20:19:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "PNxRRJRT#IRZwgVbeKGbDDdanmfiMV0hURMtTPUSN5aVaL9sYfrs",
     CAPTION: process.env.CAPTION || "POWERED BY ZERO",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
